f1c6a3b233c51bec7ae09d5f1d40367233cf229b
ArialBlack <andreyturik@gmail.com>
2016-04-20 02:30:52 +0300

    big update

--- sites/all/build/Gruntfile.js
+++ sites/all/build/Gruntfile.js
@@ -16,6 +16,27 @@ module.exports = function(grunt) {
                 }
             }
         },
+        
+        ftp_push: {
+            your_target: {
+                options: {
+		            authKey: "serverA",
+    	            host: "ftp.vdngtech.com",
+    	            dest: "/",
+    	            port: 21
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '../themes/vdnh_ui',
+                        src: [
+                        "**"
+                        ],
+                        dest: '/sites/all/themes/vdnh_ui'
+                    }
+                ]
+            }
+        },
 
         watch: {
             grunt: {
@@ -26,16 +47,38 @@ module.exports = function(grunt) {
                 files: [
                     '../themes/vdnh_ui/less/**/*.less'
                 ],
-                tasks: ['less']
+                tasks: ['less', 'postcss']
             },
+            
+            ftppush: {
+                files: [
+                    '../themes/vdnh_ui/**/*.*',
+                    '!../themes/vdnh_ui/**/*.less'
+                ],
+                tasks: ['ftp_push']
+            }
         },
+        
+        postcss: {
+            options: {
+              processors: [
+                require('autoprefixer')({browsers: ['last 2 versions', 'ie 10']}),
+              ]
+            },
+            dist: {
+              src: '../themes/vdnh_ui/css/style.css'
+            }
+        }
+        
     });
 
     // load npm modules
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ftp-push');
+    grunt.loadNpmTasks('grunt-postcss');
 
     // register tasks
-    grunt.registerTask('default', ['less', 'watch']);
-    grunt.registerTask('jenkins', ['less']);
+    grunt.registerTask('default', ['less', 'postcss', 'ftp_push', 'watch']);
+    grunt.registerTask('jenkins', ['less', 'postcss', 'ftp_push']);
 };
